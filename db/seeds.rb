6e25ddc7479ea23fdc6b4030743f6a4b230c8b41
AnoopKashyap <anup.kashyap795@gmail.com>
2019-03-25 14:05:21 +0530

    alignment of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@
 # Examples:
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # Character.create(name: 'Luke', movie: movies.first)
-User.create!([
-{ email: "anoop.kashyap@terralogic.com", password: "12345678", first_name: "Anoop", last_name:  "Kashyap", admin: true }
+User.find_or_create_by!(
+    email: "anoop.kashyap@terralogic.com", 
+    password: "12345678", 
+    first_name: "Anoop", 
+    last_name: "Kashyap", 
+    admin: true
+)
