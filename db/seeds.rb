f82d9a9b85846c4ffef2f4b0700b59d94d1345a5
AnoopKashyap <anup.kashyap795@gmail.com>
2019-04-02 18:25:04 +0530

    changed the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 # Examples:
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # Character.create(name: 'Luke', movie: movies.first)
-User.find_or_create_by!(
+user = User.find_or_create_by!(
     email: "anoop.kashyap@terralogic.com", 
-    password: "12345678", 
+    password: "12345678",
+    password_confirmation: "12345678",
     first_name: "Anoop", 
     last_name: "Kashyap", 
     admin: true
