09c3438b81a57a1d8ce57ef0e857329b85794aa9
AnoopKashyap <anup.kashyap795@gmail.com>
2019-05-02 14:49:38 +0530

    show page modified

--- spec/views/movies/show.html.haml_spec.rb
+++ spec/views/movies/show.html.haml_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'movies/show' do
   it 'displays the movie with id' do
     movie = Movie.create!(name: 'movie1', release_date: '01/02/2019', short_description: 'A specific movie', genre: 'Action', age_bar: 3)
     controller.extra_params = { id: movie.id }
-    expect(controller.request.fullpath).to eq "/movies/#{movie.id}"
+    fexpect(controller.request.fullpath).to eq "/movies/#{movie.id}"
   end
 
   it 'has user reviews button' do
